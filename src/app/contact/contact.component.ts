34526c9c63088a7528f40965e0fec50d0150ccca
Abeda Khanam <abeda2152@gmail.com>
2025-02-19 15:15:53 +0600

    form reactive

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
-export class ContactComponent {
-
+export class ContactComponent implements OnInit {
+  reactiveForm!: FormGroup; //Definite Assignment Operator to tell TypeScript it's assigned in ngOnInit()
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      firstname: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      lastname: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      gender: new FormControl(null, Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phonenumber: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[0-9]{10}$/),
+      ]),
+    });
+  }
+  onSubmit() {
+    if (this.reactiveForm.valid) {
+      console.log('Form Submitted:', this.reactiveForm.value);
+    } else {
+      console.log('Form is invalid!');
+      this.reactiveForm.markAllAsTouched(); 
+    }
+  }
 }
